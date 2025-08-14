ac3ee0439bd4df006acede2ec730c70492855991
Luis Santos Sanabria <llsssantos1993@gmail.com>
2025-08-13 23:18:47 -0300

    @For y NgClass

--- 02-bases/src/app/app.routes.ts
+++ 02-bases/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { CounterPageComponent } from './pages/counter/counter-page.component';
 import { HeroPageComponent } from './pages/hero/hero-page.component';
+import { Component } from '@angular/core';
+import { DragonballPageComponent } from './pages/dragonball/dragonball-page.component';
 
 export const routes: Routes = [
   //hola mundo => holaMundoComponent
@@ -12,6 +14,11 @@ export const routes: Routes = [
   {
     path: 'hero',
     component: HeroPageComponent,
+  },
+
+  {
+    path: 'dragonball',
+    component: DragonballPageComponent,
   },
 
   {
---
+++ 02-bases/src/app/pages/dragonball/dragonball-page.component.ts
@@ -0,0 +1,29 @@
+import { NgClass } from '@angular/common';
+import { Component, computed, signal } from '@angular/core';
+
+interface Character {
+  id: number;
+  name: string;
+  power: number;
+}
+
+@Component({
+  templateUrl: './dragonball-page.component.html',
+  //imports: [NgClass]
+})
+
+export class DragonballPageComponent {
+
+  characters = signal<Character[]>([
+    {id: 1, name: 'Goku', power:9001},
+    {id: 2, name: 'Vegeta', power:8000},
+    {id: 3, name: 'Picolo', power:3000},
+  ]);
+
+  powerClasses = computed(() =>{
+    return {
+      'text-danger': true,
+    }
+  })
+
+}
