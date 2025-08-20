a1e5687412f030f5e0f1f55b97d52ace44aac4d9
Luis Santos Sanabria <llsssantos1993@gmail.com>
2025-08-19 23:01:51 -0300

    @If-Imput

--- 02-bases/src/app/pages/dragonball/dragonball-page.component.ts
+++ 02-bases/src/app/pages/dragonball/dragonball-page.component.ts
@@ -13,17 +13,42 @@ interface Character {
 })
 
 export class DragonballPageComponent {
+  name = signal('Gohan');
+  power = signal(100);
+
 
   characters = signal<Character[]>([
     {id: 1, name: 'Goku', power:9001},
     {id: 2, name: 'Vegeta', power:8000},
     {id: 3, name: 'Picolo', power:3000},
+    {id: 4, name: 'Yancha', power:500},
   ]);
 
-  powerClasses = computed(() =>{
+  /*powerClasses = computed(() =>{
     return {
       'text-danger': true,
     }
-  })
+  })*/
 
+  addCharacter() {
+    if (!this.name() || !this.power() || this.power() <= 0 ) {
+      return;
+    }
+
+    const newCharacter:Character = {
+      id: this.characters().length + 1,
+      name: this.name(),
+      power: this.power(),
+    };
+
+    //this.characters().push(newCharacter);
+    //Forma recomenda de agregar un nuevo caracter
+    this.characters.update((list) => [...list, newCharacter]);
+    this.reset();
+}
+
+reset() {
+  this.name.set('');
+  this.power.set(0);
+}
 }
