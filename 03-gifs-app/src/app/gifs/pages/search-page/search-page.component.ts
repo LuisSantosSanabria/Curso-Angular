d473c34cee9f99e74db3b0ff76697255c23add11
Luis Santos Sanabria <llsssantos1993@gmail.com>
2025-10-13 23:20:29 -0300

    Buscador de Gifs

--- 03-gifs-app/src/app/gifs/pages/search-page/search-page.component.ts
+++ 03-gifs-app/src/app/gifs/pages/search-page/search-page.component.ts
@@ -1,8 +1,22 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { GifListComponent } from "../../components/gif-list/gif-list.component";
+import { GifService } from '../../services/gifs.service';
+import { Gif } from '../../interfaces/gif.interface';
 
 @Component({
   selector: 'app-search-page',
-  imports: [],
+  imports: [GifListComponent],
   templateUrl: './search-page.component.html',
 })
-export default class SearchPageComponent { }
+export default class SearchPageComponent {
+
+  gifService = inject( GifService )
+  // los gifs que vamos a mostrar
+  gifs = signal<Gif[]>([]);
+
+   onSearch(query: string) {
+    this.gifService.searchGifs(query).subscribe((resp) => {
+      this.gifs.set(resp);
+   });
+  }
+ }
--- 03-gifs-app/src/app/gifs/services/gifs.service.ts
+++ 03-gifs-app/src/app/gifs/services/gifs.service.ts
@@ -1,9 +1,10 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable, signal } from '@angular/core';
+import { computed, inject, Injectable, signal } from '@angular/core';
 import { environment } from '@environments/environment';
 import type { GiphyResponse } from '../interfaces/giphy.interfaces';
 import { Gif } from '../interfaces/gif.interface';
 import { GifMapper } from '../mapper/gif.mapper';
+import { map, tap } from 'rxjs';
 // cuando usamos interfaces en bueno usar la palabra type
 // para que typescript sepa que es una interfaz
 
@@ -16,6 +17,13 @@ export class GifService {
   trendingGifs = signal<Gif[]>([]);
   // creamos una señal que va a tener un array de gifs
   trendingGifsLoading = signal(true);
+
+  searchHistory = signal<Record<string, Gif[]>>({});
+  // un objeto que tiene como llave un string y como valor un array de gifs
+  // el record es un tipo de dato que nos permite crear un objeto con llaves dinamicas
+  searhHistoryKeys = computed(()=> Object.keys(this.searchHistory()));
+  // computed es una señal derivada que se actualiza cuando cambia la señal original
+
 
   constructor(){
     this.loadTrendingGifs();
@@ -36,5 +44,31 @@ export class GifService {
       this.trendingGifsLoading.set(false);
       console.log({gifs});
       });
+  }
+
+  searchGifs(query: string){ // tengo un servcio que me devuelve un observable
+      return this.http.get<GiphyResponse>(`${ environment.giphyUrl }/gifs/search`, {
+      params: {
+        api_key:environment.giphyApiKey,
+        limit: 20,
+        q: query,
+      }
+     })
+     .pipe(
+      map( ({data}) => data),
+      map( (items) => GifMapper.mapGiphyItemsToGifArray(items)),
+      /* cuando usamos el pipe podemos encadenar operadores rxjs */
+      tap (items =>{ //nos va permitir manejar el historial
+        this.searchHistory.update(history => ({
+          ...history,
+          [query.toLocaleLowerCase()]: items,
+        }))
+      })
+     );
+/*      .subscribe((resp) => {
+      const gifs = GifMapper.mapGiphyItemsToGifArray(resp.data);
+
+      console.log({search:gifs});
+      }); */
   }
 }
